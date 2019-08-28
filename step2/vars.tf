1cc05f142ee9c5bd8a24dcd8b3ea155b739b43eb
Saeid <saeidpahlevan@hotmail.com>
2019-08-28 19:12:17 +0800

    added step 1 to 3

---
+++ step2/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+variable "AMIS" {
+  type = "map"
+
+  default {
+    ap-southeast-2 = "ami-075caa3491def750"
+    us-east-1      = "ami-13be557e"
+  }
+}
